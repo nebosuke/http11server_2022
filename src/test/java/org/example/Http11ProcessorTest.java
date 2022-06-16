1bc46ff63705557cb6db64431a2c4b8fcd389fae
Kensuke Ishida <kensuke@dreamarts.co.jp>
2022-06-16 14:03:45 +0900

    不要なコメントが残っていた問題を修正

--- src/test/java/org/example/Http11ProcessorTest.java
+++ src/test/java/org/example/Http11ProcessorTest.java
@@ -11,16 +11,7 @@ import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 
 public class Http11ProcessorTest {
-
-    //    GET /top.htm HTTP/1.1
-    //    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-    //Upgrade-Insecure-Requests: 1
-    //Host: abehiroshi.la.coocan.jp
-    //User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15
-    //Accept-Language: en-US,en;q=0.9
-    //Accept-Encoding: gzip, deflate
-    //Connection: keep-alive
-
+    
     @Test
     public void testParseRequest() {
         String wireProtocol = """
